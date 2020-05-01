d584af9251bd9bff117fb0fa7427ccb36d0c38ce
Hawkin Mogen <hawkinmogen@gmail.com>
2020-05-01 01:18:46 -0500

    Solution

--- Tic-Tac-Toe Result/ticTacToeResult.py
+++ Tic-Tac-Toe Result/ticTacToeResult.py
@@ -4,14 +4,19 @@
 from typing import List
 
 def checkio(result: List[str]) -> str:
+    #check for horizontal 3 in a row
     for x in result:
         if x.count('X')==3:
             return 'X'
         elif x.count('O')==3:
             return 'O'
+   
+    #check for vertical three in a row
     for x in range(0,3):
         if result[0][x]==result[1][x]==result[2][x] and result[0][x]!='.':
             return result[0][x]
+    
+    #check for diagnol three in a row
     if result[0][0]==result[1][1]==result[2][2] or result[0][2]==result[1][1]==result[2][0]:
         if result[1][1]!='.':
          return result[1][1]
